97b47750727dcd374063024f3c6a4ef92852d743
jack970 <italocod@hotmail.com>
2020-05-27 09:58:09 -0300

    update link footer

--- src/components/InnerFooter/links.js
+++ src/components/InnerFooter/links.js
@@ -1,35 +1,35 @@
 const Mapa = [
     {
         label: 'História',
-        url: '/institucional/historia',
+        url: '/historia',
     },
     {
         label: 'equipe',
-        url: '/institucional/equipe',
+        url: '/equipe',
     },
     {
         label: 'junta médica',
-        url: '/institucional/junta-medica',
+        url: '/junta-medica',
     },
     {
         label: 'portarias do ipasc',
-        url:'/publicacoes/portarias-do-ipasc',
+        url:'/portarias-do-ipasc',
     },
     {
         label: 'portarias do mps',
-        url:'/publicacoes/portarias-do-mps',
+        url:'/portarias-do-mps',
     },
     {
         label: 'legislação municipal',
-        url:'/publicacoes/legislacao-municipal',
+        url:'/legislacao-municipal',
     },
     {
         label: 'CRP',
-        url:'/publicacoes/CRP',
+        url:'/CRP',
     },
     {
         label: 'conselho de previdência',
-        url:'/publicacoes/conselho-de-previdencia',
+        url:'/conselho-de-previdencia',
     }
 ]
 
